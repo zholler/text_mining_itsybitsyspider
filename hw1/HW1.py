0a27c8bfb0411e46138740a447c3c06cf4bcbb3d
zsuzsaholler <holler.zsuzsa@gmail.com>
2016-04-22 21:31:20 +0200

    HW1 first version added.

---
+++ hw1/HW1.py
@@ -0,0 +1,186 @@
+import numpy as np
+import codecs
+import nltk
+import re
+import math
+from nltk.tokenize import wordpunct_tokenize
+from nltk import PorterStemmer
+
+
+class Document():
+    
+    """ The Doc class rpresents a class of individul documents
+    
+    """
+    
+    def __init__(self, speech_year, speech_pres, speech_text):
+        self.year = speech_year
+        self.pres = speech_pres
+        self.text = speech_text.lower()
+        self.tokens = np.array(wordpunct_tokenize(self.text))
+        
+        
+        
+    def token_clean(self,length):
+
+        """ 
+        description: strip out non-alpha tokens and tokens of length > 'length'
+        input: length: cut off length 
+        """
+
+        self.tokens = np.array([t for t in self.tokens if (t.isalpha() and len(t) > length)])
+
+
+    def stopword_remove(self, stopwords):
+
+        """
+        description: Remove stopwords from tokens.
+        input: stopwords: a suitable list of stopwords
+        """
+
+        
+        self.tokens = np.array([t for t in self.tokens if t not in stopwords])
+
+
+    def stem(self):
+
+        """
+        description: Stem tokens with Porter Stemmer.
+        """
+        
+        self.tokens = np.array([PorterStemmer().stem(t) for t in self.tokens])
+
+
+    def word_match(self, wordset):
+        """
+        description: return word count for a given set of words
+        ?stemming needed?
+        """
+        word_dict = {}
+        for word in wordset:
+            word_dict[word] = 0
+
+        for word in self.tokens:
+            if word_dict.get(word) != None:
+                 word_dict[word] = word_dict[word] + 1
+        return(word_dict)
+ 
+
+###################################################################################################
+
+class Corpus():
+    
+    """ 
+    The Corpus class represents a document collection
+     
+    """
+    def __init__(self, doc_data, stopword_file, clean_length):
+        """
+        Notice that the __init__ method is invoked everytime an object of the class
+        is instantiated
+        """
+        
+
+        #Initialise documents by invoking the appropriate class
+        self.docs = [Document(doc[0], doc[1], doc[2]) for doc in doc_data] 
+        
+        self.N = len(self.docs)
+        self.clean_length = clean_length
+        
+        #get a list of stopwords
+        self.create_stopwords(stopword_file, clean_length)
+        
+        #stopword removal, token cleaning and stemming to docs
+        self.clean_docs(2)
+        
+        #create vocabulary
+        self.corpus_tokens()
+        
+    def clean_docs(self, length):
+        """ 
+        Applies stopword removal, token cleaning and stemming to docs
+        """
+        for doc in self.docs:
+            doc.token_clean(length)
+            doc.stopword_remove(self.stopwords)
+            doc.stem()        
+    
+    def create_stopwords(self, stopword_file, length):
+        """
+        description: parses a file of stowords, removes words of length 'length' and 
+        stems it
+        input: length: cutoff length for words
+               stopword_file: stopwords file to parse
+        """
+        
+        with codecs.open(stopword_file,'r','utf-8') as f: raw = f.read()
+        
+        self.stopwords = (np.array([PorterStemmer().stem(word) 
+                                    for word in list(raw.splitlines()) if len(word) > length]))
+        
+     
+    def corpus_tokens(self):
+        """
+        description: create a set of all all tokens or in other words a vocabulary
+        """
+        
+        #initialise an empty set
+        self.token_set = set()
+        for doc in self.docs:
+            self.token_set = self.token_set.union(doc.tokens)
+
+    def document_term_matrix(self,wordset):
+        """
+        description: create a D by V array of frequency counts
+        """
+        V = len(wordset)
+        D = self.N
+        matrix = np.empty([D,V])
+        for doc , i  in zip(self.docs, range(len(self.docs))) :
+            worddict = doc.word_match(wordset)
+            for word, j in zip(wordset, range(len(wordset))):
+                matrix[i,j] = worddict[word]
+        return(matrix)
+    
+    def tf_idf(self,wordset):
+        matrix = self.document_term_matrix(wordset)
+        nonzero_matrix = matrix > 0
+        df = (nonzero_matrix).sum(axis = 0)
+        idf = np.log( self.N / df )
+        idf[np.isinf(idf)] = 0
+        tf = (1 + np.log(matrix))
+        tf[np.isinf(tf) ] = 0
+        return(tf*idf)
+    
+    def dict_rank(self, wordset, n , freq_rep = True ):
+        """
+        freq_rep = True means we use document_term_matrix, tf_idf otherwise
+        """
+        if freq_rep:
+            matrix = self.document_term_matrix(wordset)
+        else:
+            matrix = self.tf_idf(wordset)
+        matrix = matrix.sum(axis = 1)
+        ordering = matrix.sort(axis=0)
+        maxvalues = ordering[-n]
+        for doc,i in zip(self.docs, len(self.docs)):
+            newcorpus = Corpus()
+            newcorpus = newcorpus.reproduce()
+            if matrix[i] >= maxvalues:
+                
+                
+    def reproduce(self):
+        return self.__class__()
+                
+            
+        
+        
+        
+        
+            
+            
+
+###################################################################################################
+
+[ [4,6,7,1,3,12,5],[4,6,7,1,3,12,5],[4,6,7,1,3,12,5] ] # 3 docs (D) x 7 words (V)
+        
