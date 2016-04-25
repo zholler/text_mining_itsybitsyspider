518fc77ab9ade5b74e41b9310404e8efe7d2af4a
aspeijers <anneke.speijers@barcelonagse.eu>
2016-04-26 00:16:31 +0200

    question 3 completed and question 4

---
+++ hw1/HW1_anneke.py
@@ -0,0 +1,230 @@
+import numpy as np
+import codecs
+import nltk
+import re
+import math
+from nltk.tokenize import wordpunct_tokenize
+from nltk import PorterStemmer
+
+
+class Document():
+    
+    """ The Doc class rpresents a class of individul documents
+    
+    """
+    
+    def __init__(self, speech_year, speech_pres, speech_text):
+        self.year = speech_year
+        self.pres = speech_pres
+        self.text = speech_text.lower()
+        self.tokens = np.array(wordpunct_tokenize(self.text))
+        
+        
+        
+    def token_clean(self,length):
+
+        """ 
+        description: strip out non-alpha tokens and tokens of length > 'length'
+        input: length: cut off length 
+        """
+
+        self.tokens = np.array([t for t in self.tokens if (t.isalpha() and len(t) > length)])
+
+
+    def stopword_remove(self, stopwords):
+
+        """
+        description: Remove stopwords from tokens.
+        input: stopwords: a suitable list of stopwords
+        """
+
+        
+        self.tokens = np.array([t for t in self.tokens if t not in stopwords])
+
+
+    def stem(self):
+
+        """
+        description: Stem tokens with Porter Stemmer.
+        """
+        
+        self.tokens = np.array([PorterStemmer().stem(t) for t in self.tokens])
+
+
+    def word_match(self, wordset):
+        """
+        description: return word count for a given set of words
+        ?stemming needed?
+        """
+        word_dict = {}
+        for word in wordset:
+            word_dict[word] = 0
+
+        for word in self.tokens:
+            if word_dict.get(word) != None:
+                 word_dict[word] = word_dict[word] + 1
+        return(word_dict)
+ 
+
+###################################################################################################
+
+class Corpus():
+    
+    """ 
+    The Corpus class represents a document collection
+     
+    """
+    def __init__(self, doc_data, stopword_file, clean_length):
+        """
+        Notice that the __init__ method is invoked everytime an object of the class
+        is instantiated
+        """
+        
+
+        #Initialise documents by invoking the appropriate class
+        self.docs = [Document(doc[0], doc[1], doc[2]) for doc in doc_data] 
+        
+        self.N = len(self.docs)
+        self.clean_length = clean_length
+        
+        #get a list of stopwords
+        self.create_stopwords(stopword_file, clean_length)
+        
+        #stopword removal, token cleaning and stemming to docs
+        self.clean_docs(2)
+        
+        #create vocabulary
+        self.corpus_tokens()
+        
+    def clean_docs(self, length):
+        """ 
+        Applies stopword removal, token cleaning and stemming to docs
+        """
+        for doc in self.docs:
+            doc.token_clean(length)
+            doc.stopword_remove(self.stopwords)
+            doc.stem()        
+    
+    def create_stopwords(self, stopword_file, length):
+        """
+        description: parses a file of stowords, removes words of length 'length' and 
+        stems it
+        input: length: cutoff length for words
+               stopword_file: stopwords file to parse
+        """
+        
+        with codecs.open(stopword_file,'r','utf-8') as f: raw = f.read()
+        
+        self.stopwords = (np.array([PorterStemmer().stem(word) 
+                                    for word in list(raw.splitlines()) if len(word) > length]))
+        
+     
+    def corpus_tokens(self):
+        """
+        description: create a set of all all tokens or in other words a vocabulary
+        """
+        
+        #initialise an empty set
+        self.token_set = set()
+        for doc in self.docs:
+            self.token_set = self.token_set.union(doc.tokens)
+
+    def document_term_matrix(self,wordset):
+        """
+        description: create a D by V array of frequency counts
+        """
+        V = len(wordset)
+        D = self.N
+        matrix = np.empty([D,V])
+        for doc , i  in zip(self.docs, range(len(self.docs))) :
+            worddict = doc.word_match(wordset)
+            for word, j in zip(wordset, range(len(wordset))):
+                matrix[i,j] = worddict[word]
+        return(matrix)
+    
+    def tf_idf(self,wordset):
+        """
+        description: create a D by V tf_idf array
+        """
+        matrix = self.document_term_matrix(wordset)
+        nonzero_matrix = matrix > 0
+        df = (nonzero_matrix).sum(axis = 0)
+        idf = np.log( self.N / df )
+        idf[np.isinf(idf)] = 0
+        tf = (1 + np.log(matrix))
+        tf[np.isinf(tf) ] = 0
+        return(tf*idf)
+    
+    def dict_rank(self, wordset, n , freq_rep = True ):
+        """
+        freq_rep = True means we use document_term_matrix, tf_idf otherwise
+        """
+        if freq_rep:
+            matrix = self.document_term_matrix(wordset)
+        else:
+            matrix = self.tf_idf(wordset)
+        matrix_total = matrix.sum(axis = 1)
+        #duplicating an array
+        ordering = np.array(matrix_total)  
+        ordering.sort()
+        doc_list = []
+        for j in range(n):
+            maxvalues = ordering[-(j+1)]
+            #print(ordering)
+            #print(maxvalues)
+            
+            for doc,i in zip(self.docs, range(len(self.docs))):
+                if matrix_total[i] == maxvalues:
+                    doc_list.append(self.docs[i])    
+        return(doc_list)
+        
+   
+    
+    #def imported_dictionary_rank(self, dictionary, n):
+        #"""
+        #uses imported dictionary to rank the corpus
+        #non-stem version        
+        #"""           
+        #dictionary_value = []       
+        #for i in range(len(self.docs)):
+            #text = self.docs[i].text
+            #dictionary_value[i] = sum(map(lambda word: afinn.get(word, 0), text.lower().split())) 
+        
+        #return(dictionary_value)
+
+    def weighted_dict_rank(self, sentiment_dict, freq_rep = True ):
+        """
+        description: Uses an imported weighted dictionary to return a sentiment score for each speech
+        freq_rep = True means we use document_term_matrix, tf_idf otherwise
+        """
+        # !!!it appears as though the order of these matches up but I'm not sure it always will!!!!!! 
+        # perhaps we need to change into a list of tuples first...
+        wordset = sentiment_dict.keys()
+        values = afinn_dict.values()
+        
+        if freq_rep:
+            matrix = self.document_term_matrix(wordset)
+        else:
+            matrix = self.tf_idf(wordset)
+        
+        # multiply values in matrix by valence score of each word in sentiment_dict     
+        matrix_total = np.dot(matrix, values)
+        
+        # what do we need to return? My guess: year, president, score (sorted by score). ie a list of tuples
+        doc_list = []        
+        for doc, i in zip(self.docs, range(len(self.docs))):
+            doc_list.append((self.docs[i].year, self.docs[i].pres, matrix_total[i]))
+        
+        # sort list by sentiment score (descending)
+        sorted_doc_list = sorted(doc_list, key=lambda x: x[2], reverse=True)
+        return(sorted_doc_list)
+      
+        
+        
+            
+            
+
+###################################################################################################
+
+
+        
